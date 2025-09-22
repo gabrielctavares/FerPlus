e0714f2ae3e05417abac54df4d601b1d8b6866c4
Gabriel Tavares <gabrielctavares.dev@gmail.com>
2025-09-22 20:43:31 -0300

    Atualiza a taxa de aprendizado das redes.

--- src/models.py
+++ src/models.py
@@ -60,9 +60,9 @@ class ResNet18(nn.Module):
     ResNet18 adaptada para FER+ 1x64x64 inputs e num_classes finais.
     """
     learning_rate = 0.01
-    input_width = 64
-    input_height = 64
-    input_channels = 1
+    # input_width = 64
+    # input_height = 64
+    # input_channels = 1
     def __init__(self, num_classes):
         super().__init__()
 
@@ -77,6 +77,7 @@ class ResNet18(nn.Module):
 # implementar  VGG16, VGG19, DenseNet, EfficientNet, ConvNext
 # entrada com somente 1 canal e saida = num_classes
 class VGG16(nn.Module):
+    learning_rate = 0.01
     def __init__(self, num_classes):
         super().__init__()
         self.model = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
@@ -84,51 +85,52 @@ class VGG16(nn.Module):
         
         self.model.classifier[6] = nn.Linear(4096, num_classes)
 
-
-        print(self.model)
-
     def forward(self, x):
         return self.model(x)
 
 class VGG19(nn.Module):
+    learning_rate = 0.01
     def __init__(self, num_classes):
         super().__init__()
         self.model = models.vgg19(weights=models.VGG19_Weights.DEFAULT)
         self.model.features[0] = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
         self.model.classifier[6] = nn.Linear(4096, num_classes)        
-        print(self.model)
+        
     def forward(self, x):
         return self.model(x)
 
 class DenseNet(nn.Module):
+    learning_rate = 0.005
     def __init__(self, num_classes):
         super().__init__()
         self.model = models.densenet121(weights=models.DenseNet121_Weights.DEFAULT)
         self.model.features[0] = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 
         self.model.classifier = nn.Linear(1024, num_classes)
-        print(self.model)
+        
     def forward(self, x):
         return self.model(x)
 
 
 class EfficientNet(nn.Module):
+    learning_rate = 0.001
     def __init__(self, num_classes):
         super().__init__()
         self.model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT)        
         self.model.features[0][0] = nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
         self.model.classifier[1] = nn.Linear(1280, num_classes)
-        print(self.model)
+        
 
     def forward(self, x):
         return self.model(x)
 
 class ConvNext(nn.Module):
+    learning_rate = 0.001
     def __init__(self, num_classes):
         super().__init__()
         self.model = models.convnext_tiny(weights=models.ConvNeXt_Tiny_Weights.DEFAULT)
         self.model.features[0] = nn.Conv2d(1, 96, kernel_size=(4, 4), stride=(4, 4), padding=(0, 0), bias=False)
         self.model.classifier[2] = nn.Linear(768, num_classes)
-        print(self.model)
+        
     def forward(self, x):
         return self.model(x)
